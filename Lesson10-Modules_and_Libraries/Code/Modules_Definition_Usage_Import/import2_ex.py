a492996769f0ea96b0a9dcff9358fe0489723b9f
Momotov Kirill <unknownman@live.ru>
2023-03-03 12:13:01 +0300

    Added theme: Modules. Definition, usage, import

---
+++ Lesson10-Modules_and_Libraries/Code/Modules_Definition_Usage_Import/import2_ex.py
@@ -0,0 +1,7 @@
+from math import *
+from random import randint, randrange
+
+print(pi)
+print(sin(30))
+
+print(randint(1, 10))
---
+++ Lesson10-Modules_and_Libraries/Code/Modules_Definition_Usage_Import/import_ex.py
@@ -0,0 +1,8 @@
+import math
+import random as rd
+
+print(math.pi)
+
+print(math.sin(38))
+
+print(rd.randint(1, 10))
