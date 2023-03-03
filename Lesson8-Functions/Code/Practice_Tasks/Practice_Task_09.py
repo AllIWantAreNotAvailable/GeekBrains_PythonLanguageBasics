f5d009a38584fdfac267c327e4f3827c92aee932
Momotov Kirill <unknownman@live.ru>
2023-03-03 11:38:48 +0300

    Refactoring code: Practice task №09 and Practice task №10

--- Lesson8-Functions/Code/Practice_Tasks/Practice_Task_09.py
+++ Lesson8-Functions/Code/Practice_Tasks/Practice_Task_09.py
@@ -18,56 +18,51 @@ def take_damage(damage: float, health: int) -> int:
     return int(health - damage)
 
 
-def fight(*args) -> dict:
+def fight(player1: dict, player2: dict):
+    attacker = player1
+    victim = player2
+    flag = True
 
-    for i in range(100):
-        if i % 2 == 0:
-            attacker = args[0]
-            victim = args[1]
+    while 0 < attacker["health"] and 0 < victim["health"]:
+        if flag:
+            attacker = player1
+            victim = player2
         else:
-            attacker = args[1]
-            victim = args[0]
+            attacker = player2
+            victim = player1
+        flag = not flag
 
-        attack_ratio = random.randint(7, 13) / 10
-        attack_rate = round(attacker["damage"] * attack_ratio, 2)
-        victim["health"] = take_damage(damage=attack_rate,
+        victim["health"] = take_damage(damage=attacker["damage"],
                                        health=victim["health"])
-        print(f'Игрок {victim["name"]} получил(а) {attack_rate} урона,')
+        print(f'Персонаж {attacker["name"]} нанес(а) {victim["name"]} -> {attacker["damage"]} урона,')
         if victim["health"] < 1:
             print(f'этот удар был смертельным для {victim["name"]}...\n')
-            return attacker
+            return
         print(f'у {victim["name"]} осталось {victim["health"]} HP!\n')
 
 
 def get_player(bot: bool = False) -> dict:
     """
-    Функция возвращает структуру игрока типа:\n
+    Функция возвращает структуру персонажа типа:\n
     - имя ("name");\n
     - здоровье ("health");\n
     - урон ("damage").\n
-    :param bot: Если значение True, то возвращает случайного бота, иначе игрока.
-    :return: Структуру (dict) игрока.
+    :param bot: Если значение True, то возвращает случайного бота, иначе персонажа пользователя.
+    :return: type(Структура персонажа) -> <class 'dict'>.
     """
-    min_health = 80
-    max_health = 200
-    min_damage = 25
-    max_damage = 75
+    player = {}
+    min_health, max_health = 80, 200
+    min_damage, max_damage = 25, 75
 
-    player = {
-        "name": '',
-        "health": 0,
-        "damage": 0
-    }
     if not bot:
-        player["name"] = input('Задайте имя Вашему персонажу:\n>>>')
-        player["health"] = random.randint(min_health, max_health)
-        player["damage"] = random.randint(min_damage, max_damage)
+        player["name"] = input('Задайте имя Вашему персонажу:\n>>> ')
     else:
         bot_names = ['Leo', 'Max', 'Anna', 'Alex', 'Ron', 'Kate']
         name_index = random.randint(0, len(bot_names) - 1)
         player["name"] = bot_names[name_index]
-        player["health"] = random.randint(min_health, max_health)
-        player["damage"] = random.randint(min_damage, max_damage)
+
+    player["health"] = random.randint(min_health, max_health)
+    player["damage"] = random.randint(min_damage, max_damage)
 
     low_health = player["health"] < 130
     show_health = 'На вид ты какой-то хилый, всего-то' if low_health else 'Да, таких здоровых еще поискать, целых'
@@ -82,16 +77,28 @@ def get_player(bot: bool = False) -> dict:
 
 def main() -> None:
     """
-    Программа создает 2-х игроков:\n
+    Программа создает 2-х персонажей:\n
     - одного для пользователя;\n
     - другого для бота.\n
-    Игроки сражаются пока здоровье одного из них не достигнет 0.
+    Персонажи сражаются пока здоровье одного из них не достигнет 0.
     :return: None
     """
     player = get_player()
     enemy = get_player(bot=True)
-    winner = fight(player, enemy)
-    print(f'Поздравляем с победой, {winner["name"]}!')
+
+    fight(player, enemy)
+
+    player_alive = 0 < player["health"]
+    enemy_alive = 0 < enemy["health"]
+
+    if player_alive or enemy_alive:
+        if player_alive:
+            winner = player
+        else:
+            winner = enemy
+        print(f'Поздравляем с победой, {winner["name"]}!')
+    else:
+        print('В схватке пали оба ;(')
 
 
 if __name__ == '__main__':
--- Lesson8-Functions/Code/Practice_Tasks/Practice_Task_10.py
+++ Lesson8-Functions/Code/Practice_Tasks/Practice_Task_10.py
@@ -17,7 +17,7 @@
 import random
 
 
-def damage_calculation(damage: int, armor: float):
+def damage_calculation(damage: int, armor: float) -> float:
     return damage / armor
 
 
@@ -25,52 +25,55 @@ def take_damage(damage: float, health: int) -> int:
     return int(health - damage)
 
 
-def fight(*args) -> dict:
-    for i in range(100):
-        if i % 2 == 0:
-            attacker = args[0]
-            victim = args[1]
-        else:
-            attacker = args[1]
-            victim = args[0]
+def fight(player1: dict, player2: dict):
+    attacker = player1
+    victim = player2
+    flag = True
 
-        damage = round(damage_calculation(attacker["damage"], victim["armor"]))
+    while 0 < attacker["health"] and 0 < victim["health"]:
+        if flag:
+            attacker = player1
+            victim = player2
+        else:
+            attacker = player2
+            victim = player1
+        flag = not flag
+
+        critical_strike = random.randint(0, 50) / 100 + 1
         is_critical_damage = True if 70 < random.randint(0, 100) else False
-        damage += round(damage // 2 if is_critical_damage else 0)
+        player_hit = round(attacker["damage"] * critical_strike if is_critical_damage else attacker["damage"])
+        damage = round(damage_calculation(damage=player_hit,
+                                          armor=victim["armor"]))
+        damage_blocked = player_hit - damage
 
         victim["health"] = take_damage(damage=damage,
                                        health=victim["health"])
-        print(f'Игрок {victim["name"]} получил(а) {damage} {"критического " if is_critical_damage else ""}урона,')
+        print(f'Персонаж {attacker["name"]} нанес(а) {victim["name"]} -> '
+              f'{player_hit} {"критического " if is_critical_damage else ""}урона.\n'
+              f'{victim["name"]} удалось заблокировать {damage_blocked} урона.')
         if victim["health"] < 1:
-            print(f'этот удар был смертельным для {victim["name"]}...\n')
-            return attacker
-        print(f'у {victim["name"]} осталось {victim["health"]} HP!\n')
+            print(f'Но этот удар был смертельным для {victim["name"]}...\n')
+            return
+        print(f'{victim["name"]} потерял {damage} HP, у него(неё) осталось {victim["health"]} HP!\n')
 
 
 def get_player(bot: bool = False) -> dict:
     """
-    Функция возвращает структуру игрока типа:\n
+    Функция возвращает структуру персонажа типа:\n
     - имя ("name");\n
     - здоровье ("health");\n
-    - урон ("damage").\n
-    :param bot: Если значение True, то возвращает случайного бота, иначе игрока.
-    :return: Структуру (dict) игрока.
+    - урон ("damage");\n
+    - броня ("armor").\n
+    :param bot: Если значение True, то возвращает случайного бота, иначе персонажа пользователя.
+    :return: type(Структура персонажа) -> <class 'dict'>.
     """
-    min_health = 100
-    max_health = 300
-    min_damage = 50
-    max_damage = 100
-    min_armor = 0
-    max_armor = 50
+    player = {}
+    min_health, max_health = 100, 300
+    min_damage, max_damage = 50, 100
+    min_armor,  max_armor = 0, 50
 
-    player = {
-        "name": '',
-        "health": 0,
-        "damage": 0,
-        "armor": 0.0
-    }
     if not bot:
-        player["name"] = input('Задайте имя Вашему персонажу:\n>>>')
+        player["name"] = input('Задайте имя Вашему персонажу:\n>>> ')
     else:
         bot_names = ['Leo', 'Max', 'Anna', 'Alex', 'Ron', 'Kate']
         name_index = random.randint(0, len(bot_names) - 1)
@@ -88,23 +91,35 @@ def get_player(bot: bool = False) -> dict:
 
     print(f'Приветствуем тебя, {player["name"]}!\n'
           f'{show_health} {player["health"]} HP!\n'
-          f'{"Модный костюмчик!" if 0 < protection_rate else "Какой срам..."} {protection_rate}% защиты.\n'
+          f'{"Модный костюмчик!" if 10 < protection_rate else "Какой срам..."} {protection_rate}% защиты.\n'
           f'{show_damage} Урон -> {player["damage"]}\n')
     return player
 
 
 def main() -> None:
     """
-    Программа создает 2-х игроков:\n
+    Программа создает 2-х персонажей:\n
     - одного для пользователя;\n
     - другого для бота.\n
-    Игроки сражаются пока здоровье одного из них не достигнет 0.
+    Персонажи сражаются пока здоровье одного из них не достигнет 0.
     :return: None
     """
     player = get_player()
     enemy = get_player(bot=True)
-    winner = fight(player, enemy)
-    print(f'Поздравляем с победой, {winner["name"]}!')
+
+    fight(player, enemy)
+
+    player_alive = 0 < player["health"]
+    enemy_alive = 0 < enemy["health"]
+
+    if player_alive or enemy_alive:
+        if player_alive:
+            winner = player
+        else:
+            winner = enemy
+        print(f'Поздравляем с победой, {winner["name"]}!')
+    else:
+        print('В схватке пали оба ;(')
 
 
 if __name__ == '__main__':
